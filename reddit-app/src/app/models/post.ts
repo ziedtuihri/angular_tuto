609d5455903bdb42820f95e4205bfb3fb209eff7
ziedth <zied.hack@gmail.com>
2020-11-05 23:58:01 +0100

    Reddit app form

---
+++ reddit-app/src/app/models/post.ts
@@ -0,0 +1,11 @@
+export class Post{
+    title: string;
+    link: string;
+    point: number;
+
+    constructor(title:string, link:string, point:number){
+        this.title = title;
+        this.link = link;
+        this.point = point;
+    }
+}
