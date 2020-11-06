cc456979abcc4c813392060b5209c7480b556147
ziedth <zied.hack@gmail.com>
2020-11-06 23:09:59 +0100

    Data Binding Child to Parent

--- reddit-app/src/app/app.component.ts
+++ reddit-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
+import { Post } from './models/post';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+
   title = 'reddit-app';
+
+  private post: EventEmitter<Post> = new EventEmitter();
+  newPost: Post;
+
+  addPost(title: HTMLFormElement, link: HTMLFormElement){
+    this.newPost = new Post(title.value, link.value, 0);
+    this.post.emit(this.newPost);
+
+  }
 }
---
+++ reddit-app/src/app/posts/posts.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, EventEmitter } from '@angular/core';
+import { Post } from '../models/post';
+
+@Component({
+    selector: 'app-posts',
+    templateUrl: 'posts.component.html',
+    styleUrls: ['./posts.component.css']
+})
+
+export class Posts implements OnInit {
+    
+    posts: Post[];
+
+    @Input() post;  
+
+    ngOnInit(): void {
+
+        if(this.post){
+            var myPost = this.post;
+            this.post.subscribe(data => {
+                this.posts.push(data);
+            });
+        }
+
+        this.posts = [
+            new Post("My Title 1", "link1.com", 0),
+            new Post("My Title 2", "link2.com", 1),
+            new Post("My Title 3", "link3.com", 2)
+        ];
+    
+    }
+
+    constructor(){}
+
+}
